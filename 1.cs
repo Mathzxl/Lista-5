5c64e5ad74e2fca50c3c77621d9be91f27dd0129
Mathzxl <162761845+Mathzxl@users.noreply.github.com>
2024-04-19 20:46:13 -0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] vet = new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Dê o valor dos vetores");
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Dê o valor que deseja sem encontrado");
+            n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < 20; i++)
+            {
+                if(vet[i] == n)
+                {
+                    Console.WriteLine("A posição do vetor do valor informado é: " + i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random n = new Random(5);
+            int[] vet = new int[5];
+            int m = int.MaxValue, t=0, vn;
+            for(int i = 0; i < 5; i++)
+            {
+                /*Console.WriteLine("Dê um valor qualquer");
+                vet[i] = int.Parse(Console.ReadLine());*/
+                vet[i] = n.Next();
+                Console.WriteLine("Números:" + vet[i] + "\n");
+                if(vet[i] < m)
+                {
+                    m = vet[i];
+                    t = i;
+                }
+            }
+            vn = vet[0];
+            vet[0] = m;
+            vet[t] = vn;
+            Console.WriteLine("Elementos do arranjo: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(vet[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
